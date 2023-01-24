afbc75d9fed9fb25c9a66b94a881255c29270b91
sandy1017h <121610086+sandy1017h@users.noreply.github.com>
2023-01-24 17:13:38 +0530

    Add files via upload

---
+++ trainingplatform.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace standards.bunchofclasses
+{
+    internal class trainingplatform
+    {
+        public string model;
+        public trainingplatform()
+        {
+            model = "hardwork";
+        }
+        static void Main(string[]args)
+        {
+            trainingplatform skill = new trainingplatform();
+            Console.WriteLine(skill.model);
+        }
+    }
+}
